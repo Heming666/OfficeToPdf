d2d518282e2752d6acf876f8e3a2bd508933e0f0
Heming666 <hm80@vip.qq.com>
2021-04-07 14:38:43 +0800

    修改

---
+++ Common/OfficeToPDF.cs
@@ -0,0 +1,39 @@
+﻿using Aspose.Cells;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+   public static  class OfficeToPDF
+    {
+        public static void ConvertToPdf(string sourcePath, string savePath)
+        {
+            var extension = Path.GetExtension(sourcePath);
+            switch (extension)
+            {
+                case ".doc":
+                case ".docx":
+                    Aspose.Words.Document document = new Aspose.Words.Document(sourcePath);
+                    document.Save(savePath,Aspose.Words.SaveFormat.Pdf);
+                    break;
+                case ".xls":
+                case ".xlsx":
+                    var book = new Workbook(sourcePath);
+                    book.Save(savePath, SaveFormat.Pdf);
+                    break;
+                case ".ppt":
+                case ".pptx":
+                    Aspose.Slides.Presentation presentation = new Aspose.Slides.Presentation(sourcePath);
+                    presentation.Save(savePath, Aspose.Slides.Export.SaveFormat.Pdf);
+                    break;
+                default:
+                    WriteLog.AddLog("待转换的文件不是office类型的文件");
+                    break;
+            }
+        }
+    }
+}
---
+++ WordToPdf/WordToPdf/Wlword.cs
@@ -0,0 +1,44 @@
+﻿using Aspose.Words;
+using Aspose.Words.Saving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordToPdf
+{
+    class Wlword
+    {
+
+        public void WordToPDF(String from,String to)
+        {
+            try
+            {
+                Document doc = new Document(from);
+                //保存为PDF文件，此处的SaveFormat支持很多种格式，如图片，epub,rtf 等等
+
+                //权限这块的设置成不可复制
+                PdfSaveOptions saveOptions = new PdfSaveOptions();
+                // Create encryption details and set owner password.
+                PdfEncryptionDetails encryptionDetails = new PdfEncryptionDetails(string.Empty, "password", PdfEncryptionAlgorithm.RC4_128);
+                // Start by disallowing all permissions.
+                encryptionDetails.Permissions = PdfPermissions.DisallowAll;
+                // Extend permissions to allow editing or modifying annotations.
+                encryptionDetails.Permissions = PdfPermissions.ModifyAnnotations | PdfPermissions.DocumentAssembly;
+                saveOptions.EncryptionDetails = encryptionDetails;
+                // Render the document to PDF format with the specified permissions.
+                doc.Save(to , saveOptions);
+            
+                //doc.Save(to, SaveFormat.Pdf);
+                Console.WriteLine("成功!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("强行报错!");
+            }
+         
+        }
+    }
+}
