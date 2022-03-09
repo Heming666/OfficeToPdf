59ffcc5e28a8d741247155b4a30f2a2ab14aa2d5
明 何 <hm80@vip.qq.com>
2022-03-09 09:49:08 +0800

    111

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using Spire.Doc;
+using Spire.Doc.Documents;
 
 namespace ConsoleApp1
 {
@@ -7,14 +8,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var current = AppDomain.CurrentDomain.BaseDirectory;
-            var source = Path.Combine(current, "22.pptx");
-            var target = Path.Combine(current, "22.pdf");
-            Common.OfficeToPDF.ConvertToPdf(source, target);
-            source = Path.Combine(current, "fabdcf2f-5cbc-47ee-ad28-1c492b73323a.ppt");
-             target = Path.Combine(current, "11.pdf");
-            Common.OfficeToPDF.ConvertToPdf(source, target);
-
+            //var current = AppDomain.CurrentDomain.BaseDirectory;
+            //var source = Path.Combine(current, "22.pptx");
+            //var target = Path.Combine(current, "22.pdf");
+            //Common.OfficeToPDF.ConvertToPdf(source, target);
+            //source = Path.Combine(current, "fabdcf2f-5cbc-47ee-ad28-1c492b73323a.ppt");
+            // target = Path.Combine(current, "11.pdf");
+            //Common.OfficeToPDF.ConvertToPdf(source, target);
+            string path = @"C:\Users\hm80\Desktop\web前端求职简历.html";
+                Document document = new Document();
+                document.LoadFromFile(path,FileFormat.Html,XHTMLValidationType.None);
+                document.SaveToFile(@"C:\Users\hm80\Desktop\11.pdf",FileFormat.PDF);
+                document.Close();
+                document.Dispose();
 
         }
     }
